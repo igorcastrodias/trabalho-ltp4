ded329cee7520ee9a5fce55096bb7d399c4559a2
Igor Castro <igorcastro.dias@gmail.com>
2020-03-24 23:52:29 -0300

    Adicionando páginas e corrigindo links do template para angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component'
 import { HomeComponent } from './pages/home/home.component'
+import { ContactComponent } from './pages/contact/contact.component'
+import { CollectionComponent } from './pages/collection/collection.component'
+import { CartComponent } from './pages/cart/cart.component'
+import { CheckoutComponent } from './pages/checkout/checkout.component'
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component'
 
-
-
-const routes: Routes = [
-  {path: '',component: HomeComponent}
-];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'collection', component: CollectionComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'product/:id', component: ProductDetailComponent},
+    {path: 'cart', component: CartComponent},
+    {path: 'checkout', component: CheckoutComponent},
+    {path: 'confirmation', component: ConfirmationComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductDetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    CollectionComponent,
+    ConfirmationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
