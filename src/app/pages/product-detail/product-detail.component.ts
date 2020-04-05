59d03ebac3c3257608014041bb3466d02f998370
Igor Castro <igorcastro.dias@gmail.com>
2020-04-04 22:52:14 -0300

    Obtendo dados dinamicos na página de detalhe

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+
+import { ProductService } from '../../services/product/product.service'
+import { Product } from '../../services/product/product.model'
+
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+
+  product: Product
+  numero: number
 
   ngOnInit(): void {
+    this.numero = parseInt(this.route.snapshot.params['id']);
+    this.productService.getProductById(this.numero).subscribe(product => this.product = product );
   }
 
 }
