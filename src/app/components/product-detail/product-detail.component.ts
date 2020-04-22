028c8880096c4b88a1addef6b46f8dc5aa42d3d5
Igor Castro <igorcastro.dias@gmail.com>
2020-04-22 20:01:42 -0300

    Criando componente de detalhe do produto

---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product/product.service';
+import { Product } from '../../services/product/product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+
+  product: Product
+  numero: number
+
+  ngOnInit(): void {
+    this.numero = parseInt(this.route.snapshot.params['id']);
+    this.productService.getProductById(this.numero).subscribe(product => this.product = product );
+  }
+
+  addProduct(id:number){
+	  let existing = localStorage.getItem(name);
+	  let existingArray = existing ? existing.split(',') : [];
+	  existingArray.push(id.toString());
+	  localStorage.setItem(name, existingArray.toString());
+    this.router.navigate(['/cart']);
+  }
+
+}
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
-
-
-import { ProductService } from '../../services/product/product.service'
-import { Product } from '../../services/product/product.model'
-
 
 @Component({
   selector: 'app-product-detail',
@@ -14,21 +7,7 @@ import { Product } from '../../services/product/product.model'
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
-
-  product: Product
-  numero: number
-
   ngOnInit(): void {
-    this.numero = parseInt(this.route.snapshot.params['id']);
-    this.productService.getProductById(this.numero).subscribe(product => this.product = product );
   }
 
-  addProduct(id:number){
-	  let existing = localStorage.getItem(name);
-	  let existingArray = existing ? existing.split(',') : [];
-	  existingArray.push(id.toString());
-	  localStorage.setItem(name, existingArray.toString());
-    this.router.navigate(['/cart']);
-  }
 }
