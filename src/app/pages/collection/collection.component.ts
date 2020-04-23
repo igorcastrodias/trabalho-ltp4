810fef57dabf3d12709d58a67870a649dcb650c2
Igor Castro <igorcastro.dias@gmail.com>
2020-04-22 21:55:58 -0300

    Adicionando ordenação nos itens de toda coleção

--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+
 
 @Component({
   selector: 'app-collection',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(public productSerivce : ProductService) { }
 
   ngOnInit(): void {
+  }
+
+  trocarOrdenacaoItens(value:string){
+    switch(value){
+      case "2": this.productSerivce.getListProductsSortBy("price",true);
+      break;
+      case "3": this.productSerivce.getListProductsSortBy("price",false);
+      break;
+      default:this.productSerivce.getListProducts();
+        break;
+    }
+
   }
 
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { HttpClient } from '@angular/common/http';
 import {URL_RESTAPI} from '../../app.api'
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,18 @@ export class ProductService {
       this.getListProducts()   
    }
 
-  getListProducts() {
-    
+  getListProducts() { 
     this.http.get<Product[]>(URL_RESTAPI + '/products').subscribe(
+      list =>{
+        this.listProducts = list;
+
+      }
+    )
+  }
+
+  getListProductsSortBy(column : string, asc : boolean) { 
+    let query : String = '_sort='+column+'&_order='+ (asc == true ? 'asc' : 'desc');
+    this.http.get<Product[]>(URL_RESTAPI + '/products?'+query).subscribe(
       list =>{
         this.listProducts = list;
 
