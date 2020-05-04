48c241bebb0bb329dc0d6d96a3ce73a68d176b28
Igor Castro <igorcastro.dias@gmail.com>
2020-05-03 22:25:13 -0300

    Adicionando novos campos na classe Order

--- src/app/services/order/order.model.ts
+++ src/app/services/order/order.model.ts
@@ -3,11 +3,22 @@ export class Order{
     name: string;
     price: number;
     quantity: number;
+    firstNam: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    total: number;
+
 
     constructor(){
         this.idProduct = 0;
         this.name = "";
         this.price = 0;
         this.quantity = 0;
+        this.firstNam = "";
+        this.lastName = "";
+        this.email = "";
+        this.phoneNumber = "";
+        this.total = 0;
     }
 }
